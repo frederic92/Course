32870f5fc7a0111896e520b60898221e885baa7e
frederic <fredericmbea@gmail.com>
2018-07-18 14:23:30 -0400

    Update the navigation bar Added the jumbotron with text inside styled the homepage jombotron

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     added_attrs = [:username, :last_name, :first_name, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password, :password_confirmation]
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
